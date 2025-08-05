f952a10a728395b5d3bcc78d22df90f4ca2b44cd
Chen Yan Jui <ch.focke@gmail.com>
2025-08-05 14:22:37 +0800

    add mgo save

---
+++ db/mgo/save.go
@@ -0,0 +1,19 @@
+package mgo
+
+import (
+	"context"
+
+	"errors"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+func Save(ctx context.Context, doc DocInter) (string, error) {
+	collection := GetCollection(doc.C())
+	result, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		return "", errors.Join(ErrWriteFailed, err)
+	}
+	oid, _ := result.InsertedID.(bson.ObjectID)
+	return oid.Hex(), nil
+}
