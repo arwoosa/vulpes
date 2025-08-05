07708e3e3808bd3d0e80ed9e244f1ee8c5f2fc1b
Chen Yan Jui <ch.focke@gmail.com>
2025-08-05 14:22:29 +0800

    add mgo pipefind

---
+++ db/mgo/pipefind.go
@@ -0,0 +1,29 @@
+package mgo
+
+import (
+	"context"
+
+	"errors"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type MgoAggregate interface {
+	GetPipeline(q bson.M) mongo.Pipeline
+	Index
+}
+
+func PipeFind[T MgoAggregate](ctx context.Context, aggr T, filter bson.M) ([]T, error) {
+	collection := GetCollection(aggr.C())
+	sortCursor, err := collection.Aggregate(ctx, aggr.GetPipeline(filter))
+	if err != nil {
+		return nil, errors.Join(err, ErrReadFailed)
+	}
+	var slice []T
+	err = sortCursor.All(ctx, &slice)
+	if err != nil {
+		return nil, errors.Join(err, ErrReadFailed)
+	}
+	return slice, nil
+}
